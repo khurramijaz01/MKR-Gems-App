2bf1c31a65bf12b68994326e147f7f0f9a41511d
khurramijaz01 <khurramijaz2016@gmail.com>
2025-11-27 15:34:02 +0500

    Setup Expo Router

---
+++ app/cart.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CartScreen = () => {
+  return (
+    <View>
+        <Text style={{fontSize: 30}}>Cart Screen</Text>
+    </View>
+  );
+}
+
+export default CartScreen;
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
